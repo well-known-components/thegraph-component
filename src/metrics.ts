89d15e627283dcec657ebb0115f3efb58a76f159
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2023-07-24 09:54:28 -0300

    move metrics to its own file

---
+++ src/metrics.ts
@@ -0,0 +1,23 @@
+import { IMetricsComponent } from '@well-known-components/interfaces'
+
+/**
+ * Metrics declarations, needed for your IMetricsComponent
+ * @public
+ */
+export const metricDeclarations: IMetricsComponent.MetricsRecordDefinition<string> = {
+  subgraph_ok_total: {
+    help: 'Subgraph request counter',
+    type: IMetricsComponent.CounterType,
+    labelNames: ['url']
+  },
+  subgraph_errors_total: {
+    help: 'Subgraph error counter',
+    type: IMetricsComponent.CounterType,
+    labelNames: ['url']
+  },
+  subgraph_query_duration_seconds: {
+    type: IMetricsComponent.HistogramType,
+    help: 'Request duration in seconds.',
+    labelNames: ['url']
+  }
+}
